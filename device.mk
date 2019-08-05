3cce23f1f90d43919efebd6dd332950d0dbe5a0c
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2019-08-05 23:51:15 +0200

    X00T: Import binderized light HAL
    * Remove blue led support
    
    Change-Id: Ibac971507e6e95ad9d558a3eb45c4edf5c37552d

--- device.mk
+++ device.mk
@@ -172,10 +172,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/configs/keylayout/gxfp_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gxfp_input.kl \
     $(LOCAL_PATH)/configs/keylayout/cdfinger_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/cdfinger_input.kl
-    
-# Lights
-PRODUCT_PACKAGES += \
-    lights.sdm660
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
--- hidl-hals.mk
+++ hidl-hals.mk
@@ -75,8 +75,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.asus_sdm660
 
 # NFC
 PRODUCT_PACKAGES += \
