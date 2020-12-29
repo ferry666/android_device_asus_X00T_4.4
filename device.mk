a80425c26c8dddf40cb6a62c3aec097cd95537b2
intervigilium <intervigil@gmail.com>
2020-12-30 00:29:37 +0530

    X00T: Remove unnecessary packages
    * cne files are listed as prebuilts already.
    * ebtables/ethertypes/libebtc don't exist
    * netutils is already packaged by AOSP
    
    Change-Id: I130ee9f82e6fe9a6015a3820f2ca4a0dca6c8e61

--- device.mk
+++ device.mk
@@ -85,11 +85,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     GoogleCameraGo
 
-# Connectivity Engine support (CNE)
-PRODUCT_PACKAGES += \
-    cneapiclient \
-    com.quicinc.cne
-
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -165,12 +160,6 @@ PRODUCT_PACKAGES += \
 	qti-telephony-utils \
 	qti_telephony_utils.xml
 
-# IPv6
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
@@ -222,11 +211,6 @@ PRODUCT_COPY_FILES += \
 #    NfcNci \
 #    SecureElement \
 #    Tag
-
-# Netutils
-PRODUCT_PACKAGES += \
-    libandroid_net \
-    netutils-wrapper-1.0
 
 # OMX
 PRODUCT_PACKAGES += \
