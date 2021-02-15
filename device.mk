b1dbe2405073e7172d09db9d7fd71d28d0392dc3
Swamoy14 <shubhasankar14@gmail.com>
2021-02-15 21:53:39 +0530

    X00T: Build QTI thermal 2.0 HAL

--- device.mk
+++ device.mk
@@ -357,9 +357,6 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.sdm660
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf    
 
