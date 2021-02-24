5deb9c0d8fe32fb6337c4ba4e65668eac0834dcb
pkm774 <mprabhat774@gmail.com>
2021-02-24 23:11:56 +0530

    X00T: radio: Pick android.hardware.radio.deprecated@1.0 for RIL
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -309,7 +309,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rild \
     librmnetctl \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full \
+    android.hardware.radio.deprecated@1.0
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
