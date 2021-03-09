f6a80c191dbe19769d9830ced29f2716f96b4660
Swamoy14 <shubhasankar14@gmail.com>
2021-03-09 15:16:04 +0530

    manifest: Update build fingerprint to Redfin March

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -113,8 +113,8 @@ void vendor_load_properties()
 
    // Safetynet Workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210205.004 7038034 release-keys");
-    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
-    property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.007 7124944 release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.007/7124944:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.007/7124944:user/release-keys");
 
 }
