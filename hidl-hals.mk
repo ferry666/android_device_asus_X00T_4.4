9a3ae4762b3bf38bbf27ed8bcd44aa7a58380f86
Swamoy14 <shubhasankar14@gmail.com>
2021-05-31 15:47:34 +0530

    X00T: audio: Drop non-functional soundtrigger

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -18,8 +18,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.audio.effect@6.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
