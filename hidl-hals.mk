292da001a750e25286d40bf6b6cda01a4353a4e9
Swamoy14 <shubhasankar14@gmail.com>
2021-06-06 10:42:13 +0530

    X00T: Build pixel thermal HAL

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.pixel
 
 # USB
 PRODUCT_PACKAGES += \
