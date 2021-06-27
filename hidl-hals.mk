0f42c7bf8965e3c2240be2f8fc11a1528bf65758
Swamoy14 <shubhasankar14@gmail.com>
2021-06-27 13:56:28 +0530

    X00T: Revert to mock thermal from pixel
    Even after adjusting sensor name device is heating.. So switch to safer side..

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel
+    android.hardware.thermal@2.0-service.mock
 
 # USB
 PRODUCT_PACKAGES += \
